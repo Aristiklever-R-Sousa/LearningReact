ea084a4056bc844ab67bee3e9a9ba71b99a1864f
Aristiklever R. Sousa <aristikleverrodrgues@gmail.com>
2022-02-26 23:03:39 -0300

    Utilizando o hook useState para o handle com eventos

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,16 +1,37 @@
+import { useState } from 'react';
+
 function Form() {
     function cadastrarUser(e) {
         e.preventDefault();
         // alert(`Cadstrando esse cara: ${user}`);
         console.log({e: e});
+        console.log({name , pass});
     }
+
+    const [name, setName] = useState();
+    const [pass, setPass] = useState();
 
     return (
         <div>
             <h1>Meu Cadastro:</h1>
             <form onSubmit={cadastrarUser}>
                 <div>
-                    <input type="text" placeholder="Digite o seu nome"></input>
+                    <label htmlFor="name">Nome: </label>
+                    <input
+                    type="text"
+                    id="name" name="name"
+                    placeholder="Digite o seu nome"
+                    onChange={(e) => setName(e.target.value)}
+                    />
+                </div>
+                <div>
+                    <label htmlFor="password">Senha: </label>
+                    <input
+                    type="password"
+                    id="password" name="password"
+                    placeholder="Digite sua senha"
+                    onChange={(e) => setPass(e.target.value)}
+                    />
                 </div>
                 <div>
                     <input type="submit" value="Cadastrar" />
